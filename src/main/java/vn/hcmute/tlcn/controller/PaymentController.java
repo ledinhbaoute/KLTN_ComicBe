b3958469e38438586c98eb8a01ad4ad71844a4c9
Lê Đình Bảo <120027008+ledinhbaoute@users.noreply.github.com>
2024-07-16 18:52:05 +0700

    Update PaymentController.java

--- src/main/java/vn/hcmute/tlcn/controller/PaymentController.java
+++ src/main/java/vn/hcmute/tlcn/controller/PaymentController.java
@@ -51,8 +51,9 @@ public class PaymentController {
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT+7"));
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
