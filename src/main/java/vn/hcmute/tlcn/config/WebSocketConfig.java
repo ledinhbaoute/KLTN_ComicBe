5c62bfbcc4e4cd3743524b2cea4a0d37e13d0fa5
Lê Đình Bảo <120027008+ledinhbaoute@users.noreply.github.com>
2024-07-16 15:25:27 +0700

    Update WebSocketConfig.java

--- src/main/java/vn/hcmute/tlcn/config/WebSocketConfig.java
+++ src/main/java/vn/hcmute/tlcn/config/WebSocketConfig.java
@@ -23,6 +23,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setHandshakeHandler(new CustomHandshakeHandler())
                 .addInterceptors(webSocketHandshakeInterceptor)
-                .setAllowedOrigins("http://localhost:3000").withSockJS();
+                .setAllowedOrigins("http://localhost:3000","https://kltn-comic-fe.vercel.app").withSockJS();
     }
 }
